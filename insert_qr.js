1a85486303610d76b5038ca7f3dd91ae669c1826
Adam Engel <adotengel@gmail.com>
2014-02-15 09:06:25 -0500

    Functionally complete - generates a QR for each link

--- insert_qr.js
+++ insert_qr.js
@@ -2,15 +2,18 @@
 // License - GPLv3
 // Author - Adam Even Engel
 
-function add_qr() {
-    var qr_base = "https://api.qrserver.com/v1/create-qr-code/"
-    var box = document.getElementById("linkinfo");
-    var link = encodeURIComponent(document.getElementById("shortlink-text"));
-    var img_src = qr_base+"?data="+link+"&size=50x50";
-    var elem = document.createElement("img");
-    box.appendChild(document.createTextNode("QR code: "));
-    elem.setAttribute('src', img_src);
-    elem.setAttribute('alt', "Scan to open on your mobile device!");
-    box.appendChild(elem);
-}
+var qr_base = "https://api.qrserver.com/v1/create-qr-code/";
+
+var box = document.getElementsByClassName("linkinfo")[0];
+var link = encodeURIComponent(document.getElementById("shortlink-text").value);
+var img_src = qr_base+"?data="+link+"&size=50x50";
+var div = document.createElement("div");
+var elem = document.createElement("img");
+
+intro = document.createTextNode("QR code: ");
+div.appendChild(intro);
+elem.setAttribute('src', img_src);
+elem.setAttribute('alt', "Scan to open on your mobile device!");
+div.appendChild(elem);
+box.appendChild(div);
     
